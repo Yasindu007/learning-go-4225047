a704b7746ba4f64eb70f2dfb0f27bbb1d4c21161
Yasindu007 <Yasindu007@example.com>
2025-03-19 10:34:24 +0530

    hh

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	t:= time.Date(2009, time.November, 10,23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go lauched at %s\n", t)
+
+	n := time.Now()
+	fmt.Printf("The time currently is %s\n", n)
+	fmt.Printf("This object type is time %T\n", n)
+
+	fmt.Printf(n.Format(time.ANSIC) + "\n")
+
+	tomorrow := n.AddDate(0,0,1)
+
+
 
 }
