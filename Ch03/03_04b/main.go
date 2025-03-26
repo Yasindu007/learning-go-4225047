bacb9f1ba5f9a4ecc8cc2afae46b4060cc81c768
Yasindu007 <Yasindu007@example.com>
2025-03-26 16:47:15 +0000

    3rd

--- Ch03/03_04b/main.go
+++ Ch03/03_04b/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	// This is an array
-	var colors = [3]string{"Red", "Green", "Blue"}
+	// This is an slice
+	// var colors = []string{"Red", "Green", "Blue"}
+	var colors = make([]string, 0,3)
+	colors = append(colors, "Red", "Green", "Blue")
+	colors = append(colors, "Purple", "Fuchsia")
 	fmt.Println(colors)
+
+	colors = remove(colors, 1)
+	fmt.Println(colors)
+
+	sort.Strings(colors)
+	fmt.Println(colors)
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
 }
